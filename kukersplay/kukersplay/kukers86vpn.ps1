d8a666ae81b53fcd1509415331617e441fad4932
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2018-12-28 15:10:57 +0100

    Hamachi integration

--- kukersplay/kukersplay/kukers86vpn.ps1
+++ kukersplay/kukersplay/kukers86vpn.ps1
@@ -1,33 +1,13 @@
 Import-Module -Name VpnClient -Force:$true
 
-$name = "Kukers86 VPN"
-$address = Get-Content -Path "./server.txt"
+$name = "Hamachi"
 $result = "false"
 
 try
 {
-	$username = Get-Content -Path "./login.txt"
-	$plainpassword = Get-Content -Path "./pass.txt"
-
-	$vpn = Get-VpnConnection -Name $name;
-
-	if ($vpn){
-		if($vpn.ConnectionStatus -eq "Connected"){
-			rasdial $name /DISCONNECT;
-		}
-		Remove-VpnConnection -Name $name -Force:$true
-	}
-
-	Add-VpnConnection -Name $name -ServerAddress $address -TunnelType Pptp -EncryptionLevel Required -AuthenticationMethod MSChapv2 -Force:$true -RememberCredential:$true -SplitTunneling:$false 
-
-	$vpn = Get-VpnConnection -Name $name;
-	if($vpn.ConnectionStatus -eq "Disconnected"){
-		rasdial $name $username $plainpassword;
-	}
-
 	$nics = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
 	foreach ($nic in $nics) {
-		if ($nic.name -eq $name) {
+		if ($nic.name -contains $name) {
 			$props = $nic.GetIPProperties()
 			$addresses = $props.UnicastAddresses
 			foreach ($addr in $addresses) {
